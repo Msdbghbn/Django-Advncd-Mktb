49e5161a12547dfb11eefe716ceafb8e6fd5dbc5
Msdbghbn <Masood.b2010@gmail.com>
2022-07-30 09:08:43 +0430

    S3V1 post ctgry models in blog created
    posts and category models in blog app has been created.

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,25 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=255)
+    content=models.TextField()
+    status=models.BooleanField()
+    category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
+    image=models.ImageField(null=True, blank=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    created_date=models.DateTimeField(auto_now_add=True)
+    published_date=models.DateTimeField()
+    updated_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
