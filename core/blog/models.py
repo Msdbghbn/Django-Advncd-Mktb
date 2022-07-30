c1773df976c2a43d17e7d8af9c07e72607598fb1
Msdbghbn <Masood.b2010@gmail.com>
2022-07-30 22:18:20 +0430

    S3V10 get_user_model  register post & category
    best way to use user modulation is via get_user_model

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,15 +1,20 @@
-from unicodedata import category
 from django.db import models
-from accounts.models import User
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+# getting user model object
+User=get_user_model()
+
+
 class Post(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=255)
     content=models.TextField()
     status=models.BooleanField()
     category=models.ForeignKey('Category',on_delete=models.SET_NULL,null=True)
     image=models.ImageField(null=True, blank=True)
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    
 
     created_date=models.DateTimeField(auto_now_add=True)
     published_date=models.DateTimeField()
