edf79eff20e8c431f264cd201259a1821145f441
Msdbghbn <Masood.b2010@gmail.com>
2022-07-31 11:23:57 +0430

    S4V4 create own redir cls
    using django redir class and create a special one for myself

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
-from .views import indexView
+#from .views import indexView
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
 
 app_name='blog'
 
 urlpatterns = [
-    path('fbv', indexView,name='fbv-test'),
+    #path('fbv', indexView,name='fbv-test'),
     #path('cbv', TemplateView.as_view(template_name="index.html",extra_context={'name':'ali'})),
     path('cbv',views.IndexView.as_view(),name='cbv-index'),
-    path('go-to-index/', RedirectView.as_view(pattern_name='blog:cbv-index'), name='go-to-index'),
+    path('go-to-maktab/', views.RedirectToMaktab.as_view(), name='redir-to-maktab'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .models import Post
 
+
+'''# fbv view
 def indexView(request):
     return render(request, 'index.html')
-# Create your views here.
+Create your views here.
+'''
+
+''' #redirect for fbv
+from django.shortcuts import redirect
+def RedirectToMaktab(request):
+    return redirect('https://maktabkhooneh.org/')
+
+'''
 
 class IndexView(TemplateView):
     template_name='index.html'
@@ -14,3 +24,8 @@ class IndexView(TemplateView):
         context['name'] = 'ali'
         context['posts']=Post.objects.all()
         return context
+
+
+class RedirectToMaktab(RedirectView):
+    url='https://maktabkhooneh.org/'
+    
