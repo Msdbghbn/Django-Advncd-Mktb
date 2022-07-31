0478be2e746659445b83c21b965d8364ba6f7742
Msdbghbn <Masood.b2010@gmail.com>
2022-07-31 10:38:13 +0430

    S4V3 redirect to another site

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 from .views import indexView
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
 
 
 urlpatterns = [
     path('fbv', indexView,name='fbv-test'),
     #path('cbv', TemplateView.as_view(template_name="index.html",extra_context={'name':'ali'})),
-    path('cbv',views.IndexView.as_view(),name='cbv-index')
+    path('cbv',views.IndexView.as_view(),name='cbv-index'),
+    path('go-to-Maktabkhooneh/', RedirectView.as_view(url='https://www.maktabkhooneh.org/'), name='redir-to-mktb'),
 ]
