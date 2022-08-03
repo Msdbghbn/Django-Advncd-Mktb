bfe1c86ce6613a234bc99fec6c464af3f1078790
Msdbghbn <Masood.b2010@gmail.com>
2022-08-03 09:52:41 +0430

    S5V6 404 with try except

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
 from ...models import Post
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view()
 def post_list(request):
@@ -9,6 +11,9 @@ def post_list(request):
 
 @api_view()
 def post_detail(request,id):
-    post=Post.objects.get(pk=id)
-    serializer=PostSerializer(post)
-    return Response(serializer.data)
+    try:
+        post=Post.objects.get(pk=id)
+        serializer=PostSerializer(post)
+        return Response(serializer.data)
+    except Post.DoesNotExist:
+        return Response({"detail":"post does not exist"},status=status.HTTP_404_NOT_FOUND)
