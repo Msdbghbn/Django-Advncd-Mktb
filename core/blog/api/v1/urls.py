416218f396e6e1523789cb109bc0c98ab154fd97
Msdbghbn <Masood.b2010@gmail.com>
2022-08-03 09:36:25 +0430

    S5V7 serializers intro

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name='api-v1'
 
 urlpatterns = [
-    path('post/',views.post_list,name='post-list')
+    path('post/',views.post_list,name='post-list'),
+    path('post/<int:id>/',views.post_detail,name='post-detail')
 ]
 
 
