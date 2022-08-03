2aa55488a0b094cc34e75a36b8db519694e0b925
Msdbghbn <Masood.b2010@gmail.com>
2022-08-03 12:28:25 +0430

    S5V9 enter data via post (use if valid)

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,4 +9,4 @@ class PostSerializer(serializers.Serializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id','title','content','status','created_date','published_date']
+        fields = ['id','author','title','content','status','created_date','published_date']
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,3 +1,4 @@
+from django.urls import is_valid_path
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import PostSerializer
@@ -5,12 +6,21 @@ from ...models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
-@api_view()
+@api_view(["GET","POST"])
 def post_list(request):
-    #posts=Post.objects.all()
-    posts=Post.objects.filter(status=True)
-    serializer=PostSerializer(posts,many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        #posts=Post.objects.all()
+        posts=Post.objects.filter(status=True)
+        serializer=PostSerializer(posts,many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer=PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 @api_view()
 def post_detail(request,id):
