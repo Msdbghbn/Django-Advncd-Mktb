5f15c0202d7d0436e21f1720101b1ad02fba35eb
Msdbghbn <Masood.b2010@gmail.com>
2022-08-03 10:18:57 +0430

    S5V8 ModelSerializer intro

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
-
+from ...models import Post
+'''
 class PostSerializer(serializers.Serializer):
     title=serializers.CharField(max_length=255)
     id = serializers.IntegerField()
+'''
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id','title','content','status','created_date','published_date']
