6b281948535451733ce2ef3cadcbb1de2d26b5d2
Msdbghbn <Masood.b2010@gmail.com>
2022-08-05 21:56:07 +0430

    S6V13 SerializerMethodField to create absolute url

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -9,10 +9,20 @@ class PostSerializer(serializers.Serializer):
 class PostSerializer(serializers.ModelSerializer):
     snippet=serializers.ReadOnlyField(source='get_snippet')
     relative_url=serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url=serializers.SerializerMethodField(method_name='get_abs_url')
     class Meta:
         model = Post
-        fields = ['id','author','title','content','snippet','status','relative_url','created_date','published_date']
-        
+        fields = ['id','author','title','content','snippet','status','relative_url','absolute_url','created_date','published_date']
+    
+    def get_abs_url(self,obj):
+        #obj here is the output of __str__ method of Post model which returns title of the each post. so obj here is the title of each post.
+        #obj.pk returns the pk of each post.
+        request=self.context.get('request')
+        #return request.build_absolute_uri(obj)
+        return request.build_absolute_uri(obj.pk)
+
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
