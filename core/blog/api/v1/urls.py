1d6a1da9385924cabb14cfcac9ad95a1c17d3c40
Msdbghbn <Masood.b2010@gmail.com>
2022-08-05 10:52:12 +0430

    S6V8 use Routers to create urls

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('post',views.PostViewSet,basename='post')
 
 app_name='api-v1'
 
@@ -8,9 +12,11 @@ urlpatterns = [
     #path('post/',views.PostList.as_view(),name='post-list'),
     #path('post/<int:id>/',views.post_detail,name='post-detail'),
     #path('post/<int:id>/',views.PostDetail.as_view(),name='post-detail'),
-    path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
-    path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name='post-detail'),
+    #path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
+    #path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name='post-detail'),
 
 ]
+
+urlpatterns+=router.urls
 
 
