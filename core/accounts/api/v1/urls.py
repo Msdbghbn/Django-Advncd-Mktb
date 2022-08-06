99f7ff266e50c8ac2f1e8dcb6f2bad31ba956dfd
Msdbghbn <Masood.b2010@gmail.com>
2022-08-06 21:13:20 +0430

    S8V3 Registration endpoint start

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path,include
+from . import views
+
+app_name ='api-v1'
 
 
 urlpatterns = [
+    path('registration/',views.RegistrationView.as_view(),name='registration')
 
 ]
 
---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics,status
+from .serializers import RegistrationSerializer
+from rest_framework.response import Response
+
+class RegistrationView(generics.GenericAPIView):
+    serializer_class=RegistrationSerializer
+
+    def post(self, request,*args, **kwargs):
+        serializer=RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data={
+                'email':serializer.validated_data['email']
+                
+                }
+            return Response(data,status=status.HTTP_201_CREATED)
