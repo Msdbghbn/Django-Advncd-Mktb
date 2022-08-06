fdab15ca0c8fd2a6ed1bd8c6492f6d0df504050f
Msdbghbn <Masood.b2010@gmail.com>
2022-08-06 12:09:14 +0430

    S6V21 pagination was added

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -13,6 +13,7 @@ from rest_framework import viewsets
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter,OrderingFilter
+from .paginations import DefaultPagination
 
 '''
 @api_view(["GET","POST"])
@@ -134,6 +135,7 @@ class PostModelViewSet(viewsets.ModelViewSet):
     filterset_fields = ['category','author','status']
     search_fields=['title','content']
     ordering_fields=['published_date']
+    pagination_class = DefaultPagination
 
     # @action(methods=['get'],detail=False)
     # def get_ok(self,request):
