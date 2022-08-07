411f818652e8e2730471bd74a1e56054a6204a78
Msdbghbn <Masood.b2010@gmail.com>
2022-08-07 12:23:27 +0430

    S8V7 Token Authentication (response) + override view and serializer
    override view and serializer to have costume one.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path,include
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+#from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name ='api-v1'
 
 
 urlpatterns = [
     path('registration/',views.RegistrationView.as_view(),name='registration'),
-    path('token-auth/', ObtainAuthToken.as_view(),name='token-login')
+    path('token/login/', views.CustomAuthToken.as_view(),name='token-login')
 
 ]
 
