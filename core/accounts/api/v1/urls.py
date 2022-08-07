3de23c828ba070f53395ea106f0c02ef0c3bfeda
Msdbghbn <Masood.b2010@gmail.com>
2022-08-07 12:00:54 +0430

    S8V6 Token Authentication (Login) Used ObtainAuthToken

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path,include
 from . import views
+from rest_framework.authtoken.views import ObtainAuthToken
 
 app_name ='api-v1'
 
 
 urlpatterns = [
-    path('registration/',views.RegistrationView.as_view(),name='registration')
+    path('registration/',views.RegistrationView.as_view(),name='registration'),
+    path('token-auth/', ObtainAuthToken.as_view(),name='token-login')
 
 ]
 
