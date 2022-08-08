de71f7603703bcafd6bbf6df6470d786898354e5
Msdbghbn <Masood.b2010@gmail.com>
2022-08-08 11:51:28 +0430

    S8V12 customization of JWT

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('registration/',views.RegistrationView.as_view(),name='registration'),
     path('token/login/', views.CustomAuthToken.as_view(),name='token-login'),
     path('token/logout/',views.CustomDiscardAuthToken.as_view(),name='token-logout'),
-    path('jwt/create/',TokenObtainPairView.as_view(),name='jwt-create'),
+    path('jwt/create/',views.CustomTokenObtainPairView.as_view(),name='jwt-create'),
     path('jwt/refresh/',TokenRefreshView.as_view(),name='jwt-refresh'),
     path('jwt/verify/',TokenVerifyView.as_view(),name='jwt-verify'),
 
