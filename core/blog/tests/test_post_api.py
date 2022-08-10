1e7194d89ef803c7274a5b2e91c1b7c46c02a457
Msdbghbn <Masood.b2010@gmail.com>
2022-08-10 21:48:18 +0430

    S10V11 pytest for post-list
    bcz of login required the result of test was not true and it returned 401 instead of 200!

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,5 +1,11 @@
 import pytest
+from rest_framework.test import APIClient
+from django.urls import reverse
 
+@pytest.mark.django_db
 class TestPostApi():
     def test_get_post_response_200(self):
-        assert 1 == 1
+        client=APIClient()
+        url=reverse('blog:api-v1:post-list') #'http://127.0.0.1:8000/blog/api/v1/post/'
+        response=client.get(url)
+        assert response.status_code == 200
