134efd02e555f7dbf33127ae6c1e086885ce5de2
Msdbghbn <Masood.b2010@gmail.com>
2022-08-10 21:57:18 +0430

    S10V12 create post test

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -1,11 +1,22 @@
 import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
-
+from datetime import datetime
 @pytest.mark.django_db
 class TestPostApi():
+    client=APIClient()
     def test_get_post_response_200(self):
-        client=APIClient()
         url=reverse('blog:api-v1:post-list') #'http://127.0.0.1:8000/blog/api/v1/post/'
-        response=client.get(url)
-        assert response.status_code == 200
+        response=self.client.get(url)
+        assert response.status_code == 401
+    
+    def test_create_post_response_401_status(self):
+        url=reverse('blog:api-v1:post-list')
+        data={
+            'title':'test',
+            'content':'description',
+            'status':'True',
+            'published_data':datetime.now()
+            }
+        response=self.client.post(url,data)
+        assert response.status_code == 401
