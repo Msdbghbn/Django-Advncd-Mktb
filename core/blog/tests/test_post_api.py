11a9f32941888ed5abc15b43089a530706efbb75
Msdbghbn <Masood.b2010@gmail.com>
2022-08-10 22:38:40 +0430

    S10V15 api_client and common_user fixtures + force_login force_authenticate
    I do not know why test_create_post_response_201_status function did not worked properly and responded 400 instead of 201.

--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -2,21 +2,50 @@ import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
 from datetime import datetime
+from accounts.models import User
+
+@pytest.fixture
+def api_client():
+    client=APIClient()
+    return client
+
+@pytest.fixture
+def common_user():
+    user=User.objects.create_user(email='masood.b2010@gmail.com',password='@007Leonardoo',is_verified=True)
+    return user
+
+
+
 @pytest.mark.django_db
 class TestPostApi():
-    client=APIClient()
-    def test_get_post_response_200(self):
+ 
+    def test_get_post_response_200(self,api_client,common_user):
         url=reverse('blog:api-v1:post-list') #'http://127.0.0.1:8000/blog/api/v1/post/'
-        response=self.client.get(url)
-        assert response.status_code == 401
+        user=common_user
+        api_client.force_login(user=user)
+        response=api_client.get(url)
+        assert response.status_code == 200
     
-    def test_create_post_response_401_status(self):
+    def test_create_post_response_401_status(self,api_client):
         url=reverse('blog:api-v1:post-list')
         data={
             'title':'test',
             'content':'description',
             'status':'True',
-            'published_data':datetime.now()
+            'published_date':datetime.now()
             }
-        response=self.client.post(url,data)
+        response=api_client.post(url,data)
         assert response.status_code == 401
+
+    def test_create_post_response_201_status(self,api_client,common_user):
+        url=reverse('blog:api-v1:post-list')
+        data={
+            'title':'test',
+            'content':'description',
+            'status':True,
+            'published_date':datetime.now()
+            }   
+        user=common_user
+        api_client.force_authenticate(user=user)
+        response=api_client.post(url,data)
+        assert response.status_code == 201
