718900cb90e177182d73e352cfc31ffd5274798b
Msdbghbn <Masood.b2010@gmail.com>
2022-08-10 10:25:44 +0430

    S10V3 test for url of post list and post detail

--- core/blog/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase,SimpleTestCase
-from django.urls import reverse,resolve
-from .views import IndexView
-# Create your tests here.
-class TestUrl(TestCase):
-
-    def test_blog_index_url_resolver(self):
-        url=reverse('blog:index')
-        self.assertEqual(resolve(url).func.view_class,IndexView)
