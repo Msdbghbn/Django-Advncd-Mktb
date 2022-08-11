541d63b7fccda3456915171f2a2570ee3f3a1fdc
Msdbghbn <Masood.b2010@gmail.com>
2022-08-11 12:15:48 +0430

    S11V4 apply some changes to dev branch

--- core/accounts/api/v1/urls/profiles.py
+++ core/accounts/api/v1/urls/profiles.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .. import views
 
 # from rest_framework.authtoken.views import ObtainAuthToken
-
+# minor change applied.
 
 urlpatterns = [
     path("", views.ProfileApiView.as_view(), name="profile"),
