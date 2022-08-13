6e9ef8562f139a06970474d0f5456ce5d3f0ff2c
Msdbghbn <Masood.b2010@gmail.com>
2022-08-13 09:52:41 +0430

    S16V5 caching sample

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse,JsonResponse
 import time 
 from .tasks import sendEmail
 import requests
+from django.core.cache import cache 
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
 
-def test(self):
-    response=requests.get("https://87faf5ed-d0d1-4761-82ac-04dfdc382f6c.mock.pstmn.io/test/delay/5")
-    return JsonResponse(response.json())
+def test(request):
+    if cache.get("test_delay_api") is None:
+        response = requests.get("https://87faf5ed-d0d1-4761-82ac-04dfdc382f6c.mock.pstmn.io/test/delay/5")
+        cache.set("test_delay_api",response.json(),60)
+    return JsonResponse(cache.get("test_delay_api"))
--- core/core/settings.py
+++ core/core/settings.py
@@ -202,6 +202,7 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://redis:6379/2",
+        # "TIMEOUT":600,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
