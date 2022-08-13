f7aa230442166c277079d9a9a78a8155527e8a81
Msdbghbn <Masood.b2010@gmail.com>
2022-08-13 10:00:03 +0430

    S16V6 use cache_page decorator to cache

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -4,13 +4,15 @@ import time
 from .tasks import sendEmail
 import requests
 from django.core.cache import cache 
+from django.views.decorators.cache import cache_page
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
 
+@cache_page(60)
 def test(request):
-    if cache.get("test_delay_api") is None:
-        response = requests.get("https://87faf5ed-d0d1-4761-82ac-04dfdc382f6c.mock.pstmn.io/test/delay/5")
-        cache.set("test_delay_api",response.json(),60)
-    return JsonResponse(cache.get("test_delay_api"))
+    response = requests.get(
+        "https://87faf5ed-d0d1-4761-82ac-04dfdc382f6c.mock.pstmn.io/test/delay/5"
+    )
+    return JsonResponse(response.json())
