23554c944b7153b8ef1945bf5911afeec2200389
Msdbghbn <Masood.b2010@gmail.com>
2022-08-13 09:26:57 +0430

    S16V3 create mock server in postman + create test url

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 import time 
 from .tasks import sendEmail
-
+import requests
 
 def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
+
+def test(self):
+    response=requests.get("https://87faf5ed-d0d1-4761-82ac-04dfdc382f6c.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
